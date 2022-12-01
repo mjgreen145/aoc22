12e73f5c4ff698e7f9cc408e0be8309223547b75
mjgreen145 <mjgreen145@gmail.com>
2022-12-01 13:45:47 +0000

    Day1

--- day1/calories.ts
+++ day1/calories.ts
@@ -1,7 +1,27 @@
+import { readFileSync } from 'fs';
+import * as path from 'path';
 
+function sum(x: number, y: number): number {
+  return x + y;
+}
 
 async function run() {
+  const allText = readFileSync(path.resolve(__dirname, './calories.txt'), { encoding: 'utf8' });
 
+  const totals = allText.split('\n\n').map((text) =>
+    text
+      .split('\n')
+      .map((numStr) => parseInt(numStr))
+      .reduce(sum, 0),
+  );
+
+  // Part 1
+  console.log(Math.max(...totals));
+
+  const sortedTotalsDesc = totals.sort((a: number, b: number) => b - a);
+
+  // Part 2
+  console.log(sortedTotalsDesc[0] + sortedTotalsDesc[1] + sortedTotalsDesc[2]);
 }
 
 (async () => {
